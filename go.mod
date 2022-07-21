5d56ccac1c7e140ece4a8843fa0d75d367e81738
isaqueveras <isaque.verasccb@gmail.com>
2022-07-21 11:48:59 -0300

    feat(email): :package: finalizing email sending module #7

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/isaqueveras/power-sso
 go 1.18
 
 require (
+	github.com/domodwyer/mailyak/v3 v3.3.3 // indirect
 	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.8.1
+	github.com/microcosm-cc/bluemonday v1.0.19 // indirect
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
 )
--- tools/mailer/smtp.go
+++ tools/mailer/smtp.go
@@ -1,1 +1,72 @@
 package mailer
+
+import (
+	"fmt"
+	"io"
+	"net/mail"
+	"net/smtp"
+	"regexp"
+	"strings"
+
+	"github.com/domodwyer/mailyak/v3"
+	"github.com/microcosm-cc/bluemonday"
+)
+
+var _ Mailer = (*SmtpClient)(nil)
+
+// regex to select all tabs
+var tabsRegex = regexp.MustCompile(`\t+`)
+
+// SmtpClient defines a SMTP mail client structure that implements
+type SmtpClient struct {
+	host     string
+	port     int
+	username string
+	password string
+	tls      bool
+}
+
+// NewSmtpClient creates new `SmtpClient` with the provided configuration.
+func NewSmtpClient(host string, port int, username string, password string, tls bool) *SmtpClient {
+	return &SmtpClient{
+		host:     host,
+		port:     port,
+		username: username,
+		password: password,
+		tls:      tls,
+	}
+}
+
+// Send implements `mailer.Mailer` interface.
+func (m *SmtpClient) Send(fromEmail mail.Address, toEmail mail.Address, subject string, htmlBody string, attachments map[string]io.Reader) (err error) {
+	var (
+		email    *mailyak.MailYak
+		smtpAuth = smtp.PlainAuth("", m.username, m.password, m.host)
+	)
+
+	if m.tls {
+		if email, err = mailyak.NewWithTLS(fmt.Sprintf("%s:%d", m.host, m.port), smtpAuth, nil); err != nil {
+			return err
+		}
+	} else {
+		email = mailyak.New(fmt.Sprintf("%s:%d", m.host, m.port), smtpAuth)
+	}
+
+	if fromEmail.Name != "" {
+		email.FromName(fromEmail.Name)
+	}
+
+	email.From(fromEmail.Address)
+	email.To(toEmail.Address)
+	email.Subject(subject)
+	email.HTML().Set(htmlBody)
+
+	policy := bluemonday.StrictPolicy()
+	email.Plain().Set(strings.TrimSpace(tabsRegex.ReplaceAllString(policy.Sanitize(htmlBody), "")))
+
+	for name, data := range attachments {
+		email.Attach(name, data)
+	}
+
+	return email.Send()
+}
