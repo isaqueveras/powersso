c6f6dec52a66f41ee5322ef6cb87e971575643e9
Isaque Veras <isaqueveras@alu.ufc.br>
2022-07-27 17:09:09 -0300

    feat(tools): :package: create module security #8

---
+++ internal/tools/security/random_test.go
@@ -0,0 +1,34 @@
+package security_test
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/isaqueveras/power-sso/internal/tools/security"
+)
+
+func TestRandomString(t *testing.T) {
+	generated := []string{}
+	reg := regexp.MustCompile(`[a-zA-Z0-9]+`)
+
+	for i := 0; i < 30; i++ {
+		length := 5 + i
+		result := security.RandomString(length)
+
+		if len(result) != length {
+			t.Errorf("(%d) Expected the length of the string to be %d, got %d", i, length, len(result))
+		}
+
+		if match := reg.MatchString(result); !match {
+			t.Errorf("(%d) The generated strings should have only [a-zA-Z0-9]+ characters, got %q", i, result)
+		}
+
+		for _, str := range generated {
+			if str == result {
+				t.Errorf("(%d) Repeating random string - found %q in \n%v", i, result, generated)
+			}
+		}
+
+		generated = append(generated, result)
+	}
+}
