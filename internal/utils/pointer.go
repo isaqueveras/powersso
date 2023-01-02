1c458d5daeee28a0d7fbf22b4002201c207114ed
Isaque Véras <isaqueveras@alu.ufc.br>
2023-01-02 12:11:47 -0300

    feat: get pointer

---
+++ internal/utils/pointer.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 Isaque Veras
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package utils
+
+// GetStringPointer returns a pointer reference
+func GetStringPointer(value string) *string {
+	return &value
+}
