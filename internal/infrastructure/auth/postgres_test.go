c3e073fd23b7fab1ca9a8d5ebd802843191b6b74
Isaque Véras <isaqueveras@alu.ufc.br>
2023-01-02 12:13:57 -0300

    test: mocking test in the infra layer

--- internal/infrastructure/auth/postgres_test.go
+++ internal/infrastructure/auth/postgres_test.go
@@ -7,57 +7,73 @@ package auth
 import (
 	"context"
 	"log"
+	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/isaqueveras/power-sso/internal/domain/auth"
+	"github.com/isaqueveras/power-sso/internal/utils"
 	"github.com/isaqueveras/power-sso/pkg/database/postgres"
 	"github.com/isaqueveras/power-sso/pkg/oops"
 )
 
-func TestAuth(t *testing.T) {
+func TestAuthInfrastructure(t *testing.T) {
 	suite.Run(t, new(authSuite))
 }
 
 type authSuite struct {
 	pg   *pgAuth
 	mock sqlmock.Sqlmock
+	ctx  context.Context
 
 	suite.Suite
 }
 
-func (f *authSuite) SetupTest() {
-	f.pg = new(pgAuth)
+func (a *authSuite) SetupTest() {
+	a.pg = new(pgAuth)
+	a.ctx = context.Background()
 
 	var err error
-	if f.mock, err = postgres.OpenConnectionsForTests(); err != nil {
-		f.Assert().FailNow(err.Error())
+	if a.mock, err = postgres.OpenConnectionsForTests(); err != nil {
+		a.Assert().FailNow(err.Error())
 	}
 }
 
-func (f *authSuite) SetupSuite() {
+func (a *authSuite) SetupSuite() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
-func (f *authSuite) TearDownTest() {
+func (a *authSuite) TearDownTest() {
 	postgres.CloseConnections()
 }
 
 func (a *authSuite) TestShouldCreateUser() {
+	var (
+		err    error
+		userID *string
+		input  = &auth.Register{
+			FirstName: utils.GetStringPointer("Ayrton"),
+			LastName:  utils.GetStringPointer("Senna"),
+			Email:     utils.GetStringPointer("ayrton.senna@powersso.io"),
+			Password:  utils.GetStringPointer("$2a$12$7scJnkljH5misH./.qM0YeZi7sFEU4nu4fHqOtMqHbi/p5MmzIxpG"),
+			Roles:     utils.GetStringPointer("[read:activation_token]"),
+			City:      utils.GetStringPointer("São Paulo, SP"),
+			Country:   utils.GetStringPointer("BR"),
+		}
+	)
+
 	a.mock.ExpectBegin()
-	a.mock.ExpectQuery("INSERT INTO users").
-		WithArgs("Ayrton, Senna, ayrton.senna@powersso.io, f8c6f60e48bc3458bc65df99325415bd").
-		WillReturnRows()
+	a.mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO users (first_name,last_name,email,password,roles,city,country) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING "id"`)).
+		WithArgs(input.FirstName, input.LastName, input.Email, input.Password, input.Roles, input.City, input.Country).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow("9f4a65cf-099b-4ea6-b091-36a9c06ecc74"))
 
-	tx, err := postgres.NewTransaction(context.Background(), false)
+	a.pg.DB, err = postgres.NewTransaction(a.ctx, false)
+	a.Require().NotNil(a.pg.DB)
 	a.Require().Nil(err, oops.Err(err))
-	a.Require().NotNil(tx)
 
-	a.pg.DB = tx
-
-	// FIXME: finalize the test implementation
-	_, err = a.pg.register(&auth.Register{})
+	userID, err = a.pg.register(input)
+	a.Require().NotNil(userID)
 	a.Require().Nil(err, oops.Err(err))
 }
