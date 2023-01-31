b4ab7e70ba71ce7e8232c3208eee63b9b6d92c69
Isaque Véras <isaqueveras@alu.ufc.br>
2023-01-31 19:17:15 -0300

    feat: add functions of pointer

--- internal/utils/pointer.go
+++ internal/utils/pointer.go
@@ -4,7 +4,19 @@
 
 package utils
 
+import "time"
+
 // GetStringPointer returns a pointer reference
 func GetStringPointer(value string) *string {
+	return &value
+}
+
+// GetIntPointer returns a pointer reference
+func GetIntPointer(value int) *int {
+	return &value
+}
+
+// GetTimePointer returns a pointer reference
+func GetTimePointer(value time.Time) *time.Time {
 	return &value
 }
