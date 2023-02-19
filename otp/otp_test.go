463073515d4065427f5bdbedc6122d95a313b98a
Isaque Véras <isaqueveras@alu.ufc.br>
2023-02-19 11:47:04 -0300

    test: ✅ add benchmark testing on package otp

--- otp/otp_test.go
+++ otp/otp_test.go
@@ -6,31 +6,51 @@ package otp_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/isaqueveras/power-sso/otp"
 )
 
-func TestGenerateToken(t *testing.T) {
-	type input struct {
-		secret string
-		ts     int64
-	}
+var tokens = []string{
+	"J5WGCTLVNZSG6II=",
+	"JEQGW3TPO4QHS33VEB3W65LMMQQGIZLDN5SGKIDUNBUXGIDDN5SGKLBAPFXXKIDDOVZGS33VOMQQ====",
+	"IRXSA4LVMUQHM33DYOVCAZ3PON2GCPZB",
+}
 
-	scenarios := []input{
-		{secret: "J5WGCTLVNZSG6II=", ts: 41929800},
-		{secret: "JEQGW3TPO4QHS33VEB3W65LMMQQGIZLDN5SGKIDUNBUXGIDDN5SGKLBAPFXXKIDDOVZGS33VOMQQ====", ts: 41929415},
-		{secret: "IRXSA4LVMUQHM33DYOVCAZ3PON2GCPZB", ts: 41929799},
-	}
+func TestOTP(t *testing.T) {
+	t.Run("GenerateAndValidateToken", func(t *testing.T) {
+		for i := range tokens {
+			code, err := otp.GenerateToken(tokens[i], time.Now().Unix()/30)
+			if err != nil {
+				t.Error(err)
+				continue
+			}
 
-	for i := range scenarios {
-		code, err := otp.GenerateToken(scenarios[i].secret, scenarios[i].ts)
-		if err != nil {
-			t.Errorf("%v", err)
-			continue
+			if code == "" {
+				t.Error("Empty otp code")
+			}
+
+			if err := otp.ValidateToken(&tokens[i], &code); err != nil {
+				t.Error(err)
+				continue
+			}
 		}
+	})
+}
 
-		if code == "" {
-			t.Error("Empty otp code")
+func BenchmarkOTP(b *testing.B) {
+	b.Run("GenerateAndValidateToken", func(b *testing.B) {
+		for i := range tokens {
+			code, err := otp.GenerateToken(tokens[i], time.Now().Unix()/30)
+			if err != nil {
+				b.Error(err)
+				continue
+			}
+
+			if err := otp.ValidateToken(&tokens[i], &code); err != nil {
+				b.Error(err)
+				continue
+			}
 		}
-	}
+	})
 }
