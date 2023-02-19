a925e03e1e4e31fc1ecd80ca14199e276f626efc
Isaque Véras <isaqueveras@alu.ufc.br>
2023-02-19 11:49:32 -0300

    feat: 📄 add license on file top

--- internal/interface/http/auth/user/otp/router.go
+++ internal/interface/http/auth/user/otp/router.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 Isaque Veras
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package otp
 
 import "github.com/gin-gonic/gin"
