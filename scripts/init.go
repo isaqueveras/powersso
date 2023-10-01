3c2c8436f1c39b8c5b274b8d45bc7039c7be954a
Isaque Veras <isaqueveras@alu.ufc.br>
2023-09-30 22:07:12 -0300

    feat: add license comment

--- scripts/init.go
+++ scripts/init.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 Isaque Veras
+// Use of this source code is governed by MIT
+// license that can be found in the LICENSE file.
+
 package scripts
 
 import "github.com/isaqueveras/powersso/utils"
